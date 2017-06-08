25a70c0ab760a449287ebcb5bc69e83cfe2e5eba
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-06-08 03:44:06 +0000

    merged entrypoint scripts

---
+++ entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/sh -e
+
+# Enable debugging
+if [ ! -z "$DEBUG" ]; then
+    set -x
+fi
+
+# Set up environment
+. project-init.sh
+. apache-init.sh
+
+## install project with git
+if [ "$PROJECT_VCS_METHOD" == git ]; then
+    if [ -n "$PROJECT_VCS_URL" ]; then
+        cd "$PROJECT_WORKDIR"
+        git clone -b "$PROJECT_VCS_BRANCH" "$PROJECT_VCS_URL" "./$PROJECT_APPDIR"
+        chown -R $APACHE_RUN_USER:$APACHE_RUN_GROUP "./$PROJECT_APPDIR"
+        cd "./$PROJECT_APPDIR"
+        if [ -f composer.json ]; then
+            composer update
+        fi
+    fi
+
+## install project with composer
+else
+    if [ ! -z "$PROJECT_VCS_URL" ]; then
+        /usr/bin/composer config --global repositories.0 "{\"type\": \"vcs\", \"url\": \"$PROJECT_VCS_URL\"}"
+    fi
+
+    if [ ! -z "$PROJECT_NAME" ]; then
+        /usr/bin/composer create-project \
+          --stability=dev \
+          --prefer-source \
+          --no-interaction \
+          --keep-vcs \
+          $PROJECT_REPO/$PROJECT_NAME $PROJECT_WORKDIR
+    fi
+fi
+
+
+# Enable StrictHostKeyChecking (disabled in project-init)
+if [ -f ~/.ssh/config ]; then
+    sed -i "s/StrictHostKeyChecking no/StrictHostKeyChecking yes/"  ~/.ssh/config
+fi
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+        set -- apache2-foreground "$@"
+fi
+
+exec "$@"
