654f382f8a56543c5d7911ec61914390ffc528ca
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-07-19 00:15:46 +0000

    added code to create COMPOSER_HOME/config.json

--- project-init.sh
+++ project-init.sh
@@ -13,5 +13,20 @@ if [ -n "$PROJECT_VCS_HOST" ]; then
     echo "\tStrictHostKeyChecking no" >> $HOME/.ssh/config
     echo "\tPreferredAuthentications publickey" >> $HOME/.ssh/config
     [ -z "$PROJECT_VCS_RSA" ] || echo "\n\tIdentityFile $PROJECT_VCS_RSA" >> $HOME/.ssh/config
-fi
 
+    echo "{"                                           >> $COMPOSER_HOME/config.json
+    echo "    \"repositories\": ["                     >> $COMPOSER_HOME/config.json
+    echo "        {"                                   >> $COMPOSER_HOME/config.json
+    echo "            \"url\":  \"PROJECT_VCS_HOST\"," >> $COMPOSER_HOME/config.json
+    if [ -n $PROJECT_VCS_RSA ]; then
+        echo "            \"options\": {"              >> $COMPOSER_HOME/config.json
+        echo "                \"ssh2\": {"             >> $COMPOSER_HOME/config.json
+        echo "                    \"privkey_file\": \"$PROJECT_VCS_RSA\"" >> $COMPOSER_HOME/config.json
+        echo "                }"                       >> $COMPOSER_HOME/config.json
+        echo "            },"                          >> $COMPOSER_HOME/config.json
+    fi
+    echo "            \"type\": \"vcs\""               >> $COMPOSER_HOME/config.json
+    echo "        }"                                   >> $COMPOSER_HOME/config.json
+    echo "    ]"                                       >> $COMPOSER_HOME/config.json
+    echo "}"                                           >> $COMPOSER_HOME/config.json
+fi
