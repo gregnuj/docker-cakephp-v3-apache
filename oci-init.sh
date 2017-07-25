238f513e917154313f59c682663d9d02d4fb0c76
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-07-25 21:10:38 +0000

    commented out failing test condition

--- oci-init.sh
+++ oci-init.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-ORACLE_CLIENT_ZIP="${ORACLE_CLIENT_ZIP:="/opt/src/instantclient-basiclite-linux.x64-12.2.0.1.0.zip"}"
 
 if [ -n "$ENABLE_OCI8" ]; then
 
+    ORACLE_CLIENT_ZIP="${ORACLE_CLIENT_ZIP:="/opt/src/instantclient-basiclite-linux.x64-12.2.0.1.0.zip"}"
     export ORACLE_BASE="/usr/lib/instantclient_12_2"
     export LD_LIBRARY_PATH="$ORACLE_BASE"
     export TNS_ADMIN="$ORACLE_BASE" 
     export ORACLE_HOME=="$ORACLE_BASE"
 
 
-    if [ -f $ORACLE_CLIENT_ZIP ]; then
+    #if [ -f $ORACLE_CLIENT_ZIP ]; then
         unzip $ORACLE_CLIENT_ZIP -d $(readlink -m $ORACLE_BASE/../)
         ln -s $ORACLE_BASE/libclntsh.so.12.2 $(readlink -m $ORACLE_BASE/../libclntsh.so) 
         ln -s $ORACLE_BASE/libocci.so.12.2 $(readlink -m $ORACLE_BASE/../libocci.so)
@@ -19,6 +19,6 @@ if [ -n "$ENABLE_OCI8" ]; then
 
         # install oci8
         docker-php-ext-install oci8
-    fi
+    #fi
 
 fi
