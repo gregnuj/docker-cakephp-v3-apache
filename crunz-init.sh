6f2a6f30b68562c61693bf0648f3e6454af3d3e0
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-08-14 20:09:18 +0000

    modified:   crunz-init.sh

--- crunz-init.sh
+++ crunz-init.sh
@@ -7,7 +7,7 @@ fi
 
 CRON_DIR="${CRON_DIR:="/etc/cron.d"}"
 CRON_FILE="${CRON_FILE:="${CRON_DIR}/crunz"}"
-CRON_CMD="${CRON_CMD:="* * * * * root $WORKDIR/vendor/lavary/crunz/crunz schedule:list"}"
+CRON_CMD="${CRON_CMD:="* * * * * root $WORKDIR/vendor/lavary/crunz/crunz schedule:run"}"
 
 mkdir -p ${CRON_DIR}
 echo "${CRON_CMD}" > ${CRON_FILE}
