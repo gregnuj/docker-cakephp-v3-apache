74070b51f000118491f76b0e679f2656a039b60f
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-08-16 03:03:57 +0000

    added cron check

--- crunz-init.sh
+++ crunz-init.sh
@@ -8,8 +8,13 @@ fi
 CRON_DIR="${CRON_DIR:="/etc/cron.d"}"
 CRON_FILE="${CRON_FILE:="${CRON_DIR}/crunz"}"
 CRON_CMD="${CRON_CMD:="* * * * * root $WORKDIR/vendor/lavary/crunz/crunz schedule:run"}"
+CHECK_CRON="${CHECK_CRON:="true"}"
+CHECK_FILE="${CHECK_FILE:="${CRON_DIR}/check"}"
+CHECK_CMD='\5 * * * * root echo "$(date --utc +%FT%T.%3NZ) cron is running"'
 
 mkdir -p ${CRON_DIR}
 echo "${CRON_CMD}" > ${CRON_FILE}
 chmod 755 ${CRON_FILE}
+echo "${CHECK_CMD}" > ${CHECK_FILE}
+chmod 755 ${CHECK_FILE}
 
