56457aca412ac54724848ba27af44ab79f865f98
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-09-27 14:29:46 +0000

    fixed crunz_init to use pwd

--- crunz-init.sh
+++ crunz-init.sh
@@ -7,7 +7,7 @@ fi
 
 CRON_DIR="${CRON_DIR:="/etc/cron.d"}"
 CRON_FILE="${CRON_FILE:="${CRON_DIR}/crunz"}"
-CRON_CMD="${CRON_CMD:="* * * * * root $WORKDIR/vendor/lavary/crunz/crunz schedule:run"}"
+CRON_CMD="${CRON_CMD:="* * * * * root $(pwd)/vendor/lavary/crunz/crunz schedule:run"}"
 CHECK_CRON="${CHECK_CRON:="true"}"
 CHECK_FILE="${CHECK_FILE:="${CRON_DIR}/check"}"
 CHECK_CMD='\5 * * * * root echo "$(date --utc +%FT%T.%3NZ) cron is running"'
