e4d5ecc9f5f7415919aaef81de37dbec32b0920d
Junge, Gregory (gj8287) <gj8287@us.att.com>
2018-05-17 00:01:47 +0000

    removed composer form entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,59 +7,19 @@ fi
 
 export WORKDIR="$(readlink -f .)"
 
-## install project with git
-if [ "$PROJECT_VCS_METHOD" = git ]; then
-    if [ -n "$PROJECT_VCS_URL" ]; then
-        if [ ! "$(ls -A ${WORKDIR})" ]; then
-            git clone -b "$PROJECT_VCS_BRANCH" "$PROJECT_VCS_URL" "$(readlink -f .)"
-        else
-            git stash save "container restart $(date +"%F_%T")"
-            git pull
-        fi
-        if [ -f "./composer.json" ]; then
-            composer update
-        fi
-    fi
-
-## install project with composer
-elif [ "$PROJECT_VCS_METHOD" = composer ]; then
-    if [ ! -z "$PROJECT_NAME" ]; then
-        if [ ! "$(ls -A ${WORKDIR})" ]; then
-            /usr/bin/composer create-project \
-                --stability=dev \
-                --prefer-source \
-                --no-interaction \
-                --keep-vcs \
-                $PROJECT_REPO/$PROJECT_NAME:dev-$PROJECT_VCS_BRANCH "$(readlink -f ..)"
-        elif [ -f "./composer.json" ]; then
-            composer update
-        fi
-    fi
-fi
-
 # Import apache env vars
 . $APACHE_ENVVARS
 
 # Insure proper permissions
 chown -R $APACHE_RUN_USER:$APACHE_RUN_GROUP "$(readlink -f ..)"
 
+# store env 
+printenv | egrep -v '^(_|PWD|PHP)' | awk -F '=' '{print $1"=\""$2"\""}' >> /etc/environment
+
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
         set -- apache2-foreground "$@"
 fi
-
-# link env path
-if [ -n "${NETWORK_ENV}" ]; then 
-    ln -s "$(readlink -f ${WORKDIR}/../)" "$(readlink -f ${WORKDIR}/../${NETWORK_ENV})"
-fi
-
-# link service path
-if [ -n "${SERVICE_PATH}" ]; then 
-    ln -s "${WORKDIR}" "$(readlink -f ${WORKDIR}/../${SERVICE_PATH}})"
-fi
-
-# store env for reuse by cron
-printenv | egrep -v '^(_|PWD|PHP)' | awk -F '=' '{print $1"=\""$2"\""}' >> /etc/environment
 
 exec "$@"
 
