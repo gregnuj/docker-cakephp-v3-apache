972334cbcb987f48be863505c5ecdefae847e6c4
Junge, Gregory (gj8287) <gj8287@us.att.com>
2018-05-22 03:12:00 +0000

    added optional copy for swarm configs

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,16 @@ if [ ! -z "$DEBUG" ]; then
     set -x
 fi
 
+# set workdir variable internally
 export WORKDIR="$(readlink -f .)"
+
+# copy /configs files
+configs="$(find /configs/ -type f -exec ls  {} \; 2> /dev/null | awk  '!/\/\./{print substr($0,9)}')"
+if [ -n "$configs" ]; then 
+    for config in $configs; do
+        cp --preserve=all "/configs/${config}" "${config}"
+    done;
+fi
 
 # Import apache env vars
 . $APACHE_ENVVARS
@@ -13,7 +22,8 @@ export WORKDIR="$(readlink -f .)"
 # Insure proper permissions
 chown -R $APACHE_RUN_USER:$APACHE_RUN_GROUP "$(readlink -f ..)"
 
-# store env 
+
+# store env in /etc/environment
 printenv | egrep -v '^(_|PWD|PHP)' | awk -F '=' '{print $1"=\""$2"\""}' >> /etc/environment
 
 # first arg is `-f` or `--some-option`
@@ -22,4 +32,3 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 exec "$@"
-
