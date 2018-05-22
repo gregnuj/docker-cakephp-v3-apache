63011ab3977b01db31d8c99be84f08213420fe79
Greg Junge <gregnuj@gmail.com>
2018-05-22 00:05:27 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,14 @@ if [ ! -z "$DEBUG" ]; then
     set -x
 fi
 
+# copy /configs files
+configs="$(find /configs/ -type f -exec ls  {} \; 2> /dev/null | awk  '!/\/\./{print substr($0,9)}')"
+if [ -n "$configs" ]; then 
+    for config in $configs; do
+        cp --preserve=all "/configs/${config}" "${config}"
+    done;
+fi
+
 # set workdir variable internally
 export WORKDIR="$(readlink -f .)"
 
